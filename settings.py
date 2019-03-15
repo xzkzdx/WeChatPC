833f311e88f8a4dafeaed959bebfdf531396646a
xzkzdx <1968199077@qq.com>
2019-03-15 15:04:23 +0800

    修改文件名

--- settings.py
+++ settings.py
@@ -8,9 +8,10 @@ import os
 PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 
 ABS_DIR_PATH = PROJECT_PATH
+
 IMAGE_DIR_NAME = 'image'
+
 IMAGE_SAVE_PATH = os.path.join(ABS_DIR_PATH, IMAGE_DIR_NAME)
 
-if __name__ == '__main__':
-    print(ABS_DIR_PATH)
-    print(os.path.abspath(ABS_DIR_PATH))
+
+
