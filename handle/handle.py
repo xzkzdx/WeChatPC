0075bce65e8cb7f2e0963a4fb64b594dc746e125
xzkzdx <1968199077@qq.com>
2019-03-15 09:40:08 +0800

    修改handle， 及WeChat类

--- handle/handle.py
+++ handle/handle.py
@@ -24,33 +24,60 @@ class Handle(object):
 
     def get_handle_rect(self):
         """获取句柄矩形"""
-        places = win32gui.GetWindowPlacement(self.handle)
-        return places[-1]
+        # abs_position = win32gui.GetWindowRect(self.handle)[:2]
+        abs_position = win32gui.GetWindowPlacement(self.handle)[-1][:2]
+        handle_shape = win32gui.GetClientRect(self.handle)[2:]
+        # print(abs_position, handle_shape)
+        return abs_position + handle_shape
 
-    def reset_handle_rect(self, *rect):
+    def reset_handle_rect(self, *rect, not_ensure_move=False):
         """rect must has four params,
         example: (left, top, width, height)
-        if param is None or lt zero, it will not change
+        if any param is None or lt zero, it will not change
         """
         for r_ in rect:
             if not (isinstance(r_, int)) ^ (r_ is None):
                 raise ValueError("reset_rect() params must be int or None")
         if len(rect) != 4:
             raise TypeError("reset_rect() takes exactly 4 arguments (%s given)" % len(rect))
-        self.left = self.left if rect[0] is None or rect[0] < 0 else rect[0]
-        self.top = self.top if rect[1] is None or rect[1] < 0 else rect[1]
-        self.width = self.width if rect[2] is None or rect[2] < 0 else rect[2]
-        self.height = self.height if rect[3] is None or rect[3] < 0 else rect[3]
-        win32gui.MoveWindow(self.handle, self.left, self.top, self.width, self.height, True)
+        self.left = self.left if rect[0] is None else rect[0]
+        self.top = self.top if rect[1] is None else rect[1]
+        self.width = self.width if rect[2] is None else rect[2]
+        self.height = self.height if rect[3] is None else rect[3]
+        win32gui.MoveWindow(self.handle, self.left, self.top, self.width, self.height, not_ensure_move)
+
+    def change_position(self, *args, not_ensure_move: bool = False, ensure_hidden: bool = True):
+        self.show_handle()
+        self.reset_handle_rect(*args, not_ensure_move=not_ensure_move)
+        self.set_handle_min() if ensure_hidden else self.set_handle_foreground()
+
+    def set_handle_max(self):
+        """最大化句柄窗口"""
+        # self.set_handle_min()
+        win32gui.ShowWindow(self.handle, win32con.SW_MAXIMIZE)
+
+    def set_handle_min(self):
+        """最小化句柄窗口"""
+        win32gui.ShowWindow(self.handle, win32con.SW_MINIMIZE)
 
     def show_handle(self):
-        """在前台显示"""
-        win32gui.SetForegroundWindow(self.handle)
-        win32gui.ShowWindow(self.handle)
+        """显示句柄窗口"""
+        win32gui.ShowWindow(self.handle, win32con.SW_SHOWDEFAULT)
+        # self.set_handle_background()
 
     def hidden_handle(self):
+        """隐藏句柄窗口  0 """
+        # win32gui.ShowWindow(self.handle, win32con.HIDE_WINDOW)
+        win32gui.ShowWindow(self.handle, win32con.SW_HIDE)
+
+    def set_handle_foreground(self):
+        """在前台显示"""
+        # self.show_handle()
+        win32gui.SetForegroundWindow(self.handle)
+
+    def set_handle_background(self):
         """在后台显示"""
-        win32gui.SetBkMode(self.handle, win32con.TRANSPARENT)
+        win32gui.SetForegroundWindow(self.handle)
 
     def get_mouse_position(self):
         """获取鼠标位置"""
@@ -74,7 +101,23 @@ class Handle(object):
         win32clipboard.SetClipboardData(win32con.CF_TEXT, string.encode(encoding=encoding))
         win32clipboard.CloseClipboard()
 
-    def right_click_position(self, x_position, y_position, sleep_time=0.1):
+    def mouse_left_click_move(self, x_position, y_position, x_end_position, y_end_position, sleep_time=1):
+        # 将两个16位的值连接成一个32位的地址坐标
+        start_position = win32api.MAKELONG(x_position, y_position)
+        end_position = win32api.MAKELONG(x_end_position, y_end_position)
+        self.set_mouse_position(x_position, y_position)
+        time.sleep(int(sleep_time))
+        # 点击左键
+        win32api.SendMessage(self.handle, win32con.WM_LBUTTONDOWN, win32con.MK_LBUTTON, start_position)
+        self.set_mouse_position(x_end_position, y_end_position)
+        time.sleep(int(sleep_time))
+        win32api.mouse_event(win32con.MOUSEEVENTF_WHEEL, x_position, y_position, 1)
+        win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, 0, 0, -1)
+        time.sleep(int(sleep_time))
+        win32api.SendMessage(self.handle, win32con.WM_LBUTTONUP, win32con.MK_LBUTTON, end_position)
+        time.sleep(int(sleep_time))
+
+    def mouse_right_click_position(self, x_position, y_position, sleep_time=0.1):
         """鼠标右点击"""
         # 将两个16位的值连接成一个32位的地址坐标
         long_position = win32api.MAKELONG(x_position, y_position)
@@ -83,7 +126,7 @@ class Handle(object):
         win32api.SendMessage(self.handle, win32con.WM_RBUTTONUP, win32con.MK_RBUTTON, long_position)
         time.sleep(int(sleep_time))
 
-    def left_click_position(self, x_position, y_position, sleep_time=0.1):
+    def mouse_left_click_position(self, x_position, y_position, sleep_time=0.1):
         """鼠标左点击"""
         # 将两个16位的值连接成一个32位的地址坐标
         long_position = win32api.MAKELONG(x_position, y_position)
@@ -108,3 +151,16 @@ class Handle(object):
             win32api.SendMessage(self.handle, win32con.WM_SYSKEYDOWN, arg, 0)
         for arg in args:
             win32api.SendMessage(self.handle, win32con.WM_SYSKEYUP, arg, 0)
+
+    def mouse_move_up(self, x_position, y_position):
+        self.set_mouse_position(x_position, y_position)
+        win32api.mouse_event(win32con.MOUSEEVENTF_WHEEL, x_position, y_position, 1)
+
+    def mouse_move_down(self, x_position, y_position):
+        self.set_mouse_position(x_position, y_position)
+        win32api.mouse_event(win32con.MOUSEEVENTF_WHEEL, 0, 0, -1)
+
+    # def get_position_color(self, x_position, y_position):
+    #     win32api.GetHandleInformation(self.handle)
+    #     color = win32api.GetSysColor(self.handle, x_position, y_position)
+    #     return color
--- handle/wechat_handle.py
+++ handle/wechat_handle.py
@@ -2,6 +2,7 @@
 # !/usr/bin/python3
 # @Time   : 2019/3/13 17:17
 # @File   : wechat_handle.py
+import time
 
 from handle import Handle, InvalidHandleError
 
@@ -19,10 +20,7 @@ class WeChatMainWndForPCLoginHandle(Handle):
             except InvalidHandleError:
                 if title_index == len(self.class_titles):
                     raise InvalidHandleError("没有登陆窗口，请确认您的操作")
-        self.change_position(None, None, 280, 400)
-
-    def change_position(self, *args):
-        self.reset_handle_rect(*args)
+        self.change_position(None, None, 280, 400, not_ensure_move=False)
 
     def wx_handle(self):
         """获取微信句柄"""
@@ -34,7 +32,7 @@ class WeChatMainWndForPCLoginHandle(Handle):
 
     def login(self):
         """登陆"""
-        self.left_click_position(140, 280)
+        self.mouse_left_click_position(140, 280)
 
 
 class WeChatMainWndForPCHandle(Handle):
@@ -43,14 +41,43 @@ class WeChatMainWndForPCHandle(Handle):
 
     def __init__(self):
         super().__init__(self.class_name, self.class_title)
+        self.change_position(200, 100, 850, 560)
 
     def wx_handle(self):
         """获取微信句柄"""
         return self.handle
 
+    def message_list_move2top(self):
+        position_x = self.left + 305
+        position_y = self.top + 100
+        self.mouse_left_click_move(position_x, position_y, position_x, position_y + 100)
+
+        # self.message_list_range_move(10000)
+
+    def message_list_move2bottom(self):
+        self.message_list_range_move(10000, move_up=False)
+        # position_x = self.left + 180
+        # position_y = self.top + 150
+        # color = self.get_position_color(position_x, position_y)
+        # print(color)
+
+    def message_list_range_move(self, frequency: int, move_up: bool = True):
+        self.show_handle()
+        time.sleep(0.1)
+        position_x = self.left + 180
+        position_y = self.top + 150
+        for i in range(frequency):
+            self.mouse_move_up(position_x, position_y) if move_up else self.mouse_move_down(position_x, position_y)
+        self.hidden_handle()
+
 
 if __name__ == '__main__':
-    wx = WeChatMainWndForPCLoginHandle()
+    # wx = WeChatMainWndForPCLoginHandle()
+    # wx.login()
+    wx = WeChatMainWndForPCHandle()
     rect = wx.get_handle_rect()
     print(rect)
-    wx.login()
+    # wx.hidden_handle()
+    # wx.show_handle()
+    wx.message_list_move2top()
+    # wx.message_list_move2bottom()
